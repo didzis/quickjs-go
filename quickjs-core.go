c00ffe90aecb734ae67b3b973bdffc5ff57ae98d
Didzis Gosko <didzis@users.noreply.github.com>
2024-08-20 11:39:35 +0300

    Include QuickJS directly into source tree; remove 3rdparty pre-built libraries

---
+++ quickjs-core.go
@@ -0,0 +1,10 @@
+package quickjs
+
+/*
+#cgo CFLAGS: -Wall -Wextra -Wno-sign-compare -Wno-missing-field-initializers -Wundef -Wuninitialized -Wunused -Wno-unused-parameter -Wwrite-strings -Wchar-subscripts -funsigned-char -D_GNU_SOURCE -DCONFIG_BIGNUM -O2
+
+#define CONFIG_VERSION "2024-02-14"
+
+#include "quickjs/quickjs.c"
+*/
+import "C"
---
+++ quickjs-libs.c
@@ -0,0 +1,4 @@
+#include "quickjs/quickjs-libc.c"
+#include "quickjs/cutils.c"
+#include "quickjs/libregexp.c"
+#include "quickjs/libbf.c"
